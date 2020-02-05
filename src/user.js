114babf9c4b4811bfd195173d8eb2994d9c76854
Taras Tarlov <ttarlov@gmail.com>
2020-02-04 17:21:53 -0700

    Change constructor parameter from data to userData.

--- src/user.js
+++ src/user.js
@@ -1,14 +1,15 @@
-const data = require('./data/users.js');
+const userData = require('../data/users.js');
+
 
 class User {
-  constructor(data) {
-    this.id = data.id;
-    this.name = data.name;
-    this.address = data.address;
-    this.email = data.email;
-    this.strideLength = data.strideLength;
-    this.dailyStepGoal = data.dailyStepGoal;
-    this.friends = data.friends;
+  constructor(userData) {
+    this.id = userData.id;
+    this.name = userData.name;
+    this.address = userData.address;
+    this.email = userData.email;
+    this.strideLength = userData.strideLength;
+    this.dailyStepGoal = userData.dailyStepGoal;
+    this.friends = userData.friends;
   }
 
   getFirstName() {
