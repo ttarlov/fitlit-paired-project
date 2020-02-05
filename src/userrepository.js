2a387c61dd100daff5c0b0edf061b9df446924bc
Taras Tarlov <ttarlov@gmail.com>
2020-02-04 18:30:56 -0700

    Add calculateAvgStepGoal Method

--- src/userrepository.js
+++ src/userrepository.js
@@ -11,7 +11,14 @@ class UserRepository {
   }
 
   calculateAvgStepGoal() {
-
+    //need to loop through all of the users. grab their dailyStepGoal
+    //add them together and divide them by the number of users in the group.
+    let total = 0;
+    this.data.forEach(user => {
+      return total+= user.dailyStepGoal;
+    });
+    let avg = total / this.data.length;
+    return avg;
   }
 
 }
