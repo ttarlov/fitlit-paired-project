dd09f150bc87dd5aa110691707af772bf5852970
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-02-05 14:01:40 -0700

    :construction: Linked class files to HTML document; Started writing scripts.js file instantiating a random user & all the user data

--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,6 @@
-console.log("Hello World");
+let randomNumber = (Math.floor(Math.random() * 50));
+const userRepo = new UserRepository(userData)
+const user = new User(userData[`${randomNumber}`])
+
+console.log(userRepo);
+console.log(user);
