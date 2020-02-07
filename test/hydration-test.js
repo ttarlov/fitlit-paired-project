fdcf1651dc2c7af63d06fd33175bb21939efe5df
Taras Tarlov <ttarlov@gmail.com>
2020-02-07 16:23:46 -0700

    :crystal_ball: Add to the sample dataset for better represent real data

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -38,8 +38,18 @@ beforeEach(() => {
     },
     {
       "userID": 2,
-      "date": "2019/06/21",
-      "numOunces": 33
+      "date": "2019/06/22",
+      "numOunces": 30
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/23",
+      "numOunces": 45
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/24",
+      "numOunces": 48
     },
     {
       "userID": 1,
@@ -49,7 +59,47 @@ beforeEach(() => {
     {
       "userID": 1,
       "date": "2019/06/21",
+      "numOunces": 55
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/22",
+      "numOunces": 31
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/23",
+      "numOunces": 28
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/24",
+      "numOunces": 40
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/25",
+      "numOunces": 33
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/26",
+      "numOunces": 29
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/27",
       "numOunces": 37
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/28",
+      "numOunces": 44
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/29",
+      "numOunces": 51
     },
   ];
 
@@ -78,7 +128,7 @@ describe('Hydration', function() {
   describe('calculateAvgOzsPerDay Method', () => {
 
     it('should calculate average ozs consumed per day', () => {
-      expect(hydro.calculateAvgOzsPerDay()).to.equal(61);
+      expect(hydro.calculateAvgOzsPerDay()).to.equal(57);
     });
   })
 
@@ -91,10 +141,26 @@ describe('Hydration', function() {
   describe('calculateWeeklyOz Method', () => {
 
     it('should calculate how many fl oz consumed each day over the course of a week', () => {
-      expect(hydro.calculateWeeklyOz()).to.deep.equal([{ date: '2019/06/15', amount: 55 },{ date: '2019/06/16', amount: 63 },{ date: '2019/06/17', amount: 73 },{ date: '2019/06/18', amount: 85 },{ date: '2019/06/19', amount: 93 },{ date: '2019/06/20', amount: 29 },{ date: '2019/06/21', amount: 33 }]);
+      expect(hydro.calculateWeeklyOz('2019/06/24')).to.deep.equal([
+  { userID: 2, date: '2019/06/17', numOunces: 73 },
+  { userID: 2, date: '2019/06/18', numOunces: 85 },
+  { userID: 2, date: '2019/06/19', numOunces: 93 },
+  { userID: 2, date: '2019/06/20', numOunces: 29 },
+  { userID: 2, date: '2019/06/22', numOunces: 30 },
+  { userID: 2, date: '2019/06/23', numOunces: 45 },
+  { userID: 2, date: '2019/06/24', numOunces: 48 }
+]);
     });
     it('should calculate how many fl oz consumed each day over the course of a week for another user', () => {
-      expect(hydro2.calculateWeeklyOz()).to.deep.equal([{ date: '2019/06/20', amount: 22 },{ date: '2019/06/21', amount: 37}]);
+      expect(hydro2.calculateWeeklyOz('2019/06/29')).to.deep.equal([
+  { userID: 1, date: '2019/06/23', numOunces: 28 },
+  { userID: 1, date: '2019/06/24', numOunces: 40 },
+  { userID: 1, date: '2019/06/25', numOunces: 33 },
+  { userID: 1, date: '2019/06/26', numOunces: 29 },
+  { userID: 1, date: '2019/06/27', numOunces: 37 },
+  { userID: 1, date: '2019/06/28', numOunces: 44 },
+  { userID: 1, date: '2019/06/29', numOunces: 51 }
+]);
     });
   })
 
