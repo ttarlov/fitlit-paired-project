348d3f6e9d8b942b1c2a7abda27334a4f18e6b5c
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-02-08 16:40:58 -0700

    :crystal_ball: Added findMostRestedUserByDate method

--- src/sleep.js
+++ src/sleep.js
@@ -30,9 +30,34 @@ class Sleep {
   calculateWeeklySleep(date) {
     let indexOfCurrentDay = this.user.findIndex(day => day.date === date);
       return this.user.slice(indexOfCurrentDay -6, indexOfCurrentDay + 1)
+  };
+
+  calculateAvgSleepQualityForAllUsers() {
+      let totalSleepQuality = this.data.reduce((acc, user) => {
+          acc+= user.sleepQuality
+          return acc;
+        },0);
+      return  (totalSleepQuality / this.data.length).toFixed(2);
+  };
+
+  // findAllUsersWhoSleepGood() {
+  ////Find a week's worth of data for every user
+  //Calculate average sleep quality for each user for the week
+  //return only the users with an average sleep quality > 3
+  // }
+
+  findMostRestedUserByDate(date) {
+    let selectedDay = this.data.filter(day => day.date === date)
+    console.log(selectedDay.sort((a, b) => b.hoursSlept - a.hoursSlept)[0]);
+    return selectedDay.sort((a, b) => b.hoursSlept - a.hoursSlept)[0]
+    //iterate through array of objects and find user with most sleep
   }
 
-};
+  calculateTotalHoursSleptInAWeek(date) {
+
+  }
+  };
+
 
 
 
