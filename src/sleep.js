f3fdb1181744ffae77b51ea942c6a991adcc5f1c
Taras Tarlov <ttarlov@gmail.com>
2020-02-08 14:03:13 -0700

    :crystal_ball: Add Sleep Class with properties and findUserSleepData meth

---
+++ src/sleep.js
@@ -0,0 +1,18 @@
+class Sleep {
+  constructor(sleepData, id) {
+    this.data = sleepData;
+    this.id = id;
+    this.user = this.findUserSleepData();
+  };
+
+  findUserSleepData() {
+    return this.data.filter(user => user.userID === this.id);
+  }
+
+};
+
+
+
+if (typeof module !== 'undefined') {
+  module.exports = Sleep;
+}
