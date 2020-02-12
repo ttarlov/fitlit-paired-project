01178dd2eca34cf1b9f979aba8b7eed767cb5af2
Taras Tarlov <ttarlov@gmail.com>
2020-02-11 22:49:12 -0700

    :crystal_ball: Removed unneeded semicolons

--- src/hydration.js
+++ src/hydration.js
@@ -3,7 +3,7 @@ class Hydration {
     this.data = hydrationData;
     this.id = id;
     this.user = this.findUserHydroData();
-  };
+  }
 
   findUserHydroData() {
     return this.data.filter(user => user.userID === this.id);
@@ -17,18 +17,18 @@ class Hydration {
       },0);
     return Math.floor(totalOz/this.user.length)
 
-  };
+  }
 
   calculateDailyOz(date) {
     let selectedDay = this.user.find(day => day.date === date)
     return selectedDay.numOunces;
-  };
+  }
 
   calculateWeeklyOz(date) {
     let indexOfCurrentDay = this.user.findIndex(day => day.date === date);
     return this.user.slice(indexOfCurrentDay -6, indexOfCurrentDay + 1)
   }
-};
+}
 
 
 if (typeof module !== 'undefined') {
