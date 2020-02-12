fc8cbcb566671a2b4850698f263163cf3375224e
Karl Nielsen <karl.g.nielsen@gmail.com>
2020-02-12 15:59:22 -0700

    :bar_chart: cleaned up white space and removed extra comments

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -3,7 +3,6 @@ const expect = chai.expect;
 const Hydration = require('../src/hydration.js');
 const User = require('../src/user.js');
 
-
 beforeEach(() => {
   data = [
     {
@@ -142,25 +141,25 @@ describe('Hydration', function() {
 
     it('should calculate how many fl oz consumed each day over the course of a week', () => {
       expect(hydro.calculateWeeklyOz('2019/06/24')).to.deep.equal([
-  { userID: 2, date: '2019/06/17', numOunces: 73 },
-  { userID: 2, date: '2019/06/18', numOunces: 85 },
-  { userID: 2, date: '2019/06/19', numOunces: 93 },
-  { userID: 2, date: '2019/06/20', numOunces: 29 },
-  { userID: 2, date: '2019/06/22', numOunces: 30 },
-  { userID: 2, date: '2019/06/23', numOunces: 45 },
-  { userID: 2, date: '2019/06/24', numOunces: 48 }
-]);
+        { userID: 2, date: '2019/06/17', numOunces: 73 },
+        { userID: 2, date: '2019/06/18', numOunces: 85 },
+        { userID: 2, date: '2019/06/19', numOunces: 93 },
+        { userID: 2, date: '2019/06/20', numOunces: 29 },
+        { userID: 2, date: '2019/06/22', numOunces: 30 },
+        { userID: 2, date: '2019/06/23', numOunces: 45 },
+        { userID: 2, date: '2019/06/24', numOunces: 48 }
+      ]);
     });
     it('should calculate how many fl oz consumed each day over the course of a week for another user', () => {
       expect(hydro2.calculateWeeklyOz('2019/06/29')).to.deep.equal([
-  { userID: 1, date: '2019/06/23', numOunces: 28 },
-  { userID: 1, date: '2019/06/24', numOunces: 40 },
-  { userID: 1, date: '2019/06/25', numOunces: 33 },
-  { userID: 1, date: '2019/06/26', numOunces: 29 },
-  { userID: 1, date: '2019/06/27', numOunces: 37 },
-  { userID: 1, date: '2019/06/28', numOunces: 44 },
-  { userID: 1, date: '2019/06/29', numOunces: 51 }
-]);
+        { userID: 1, date: '2019/06/23', numOunces: 28 },
+        { userID: 1, date: '2019/06/24', numOunces: 40 },
+        { userID: 1, date: '2019/06/25', numOunces: 33 },
+        { userID: 1, date: '2019/06/26', numOunces: 29 },
+        { userID: 1, date: '2019/06/27', numOunces: 37 },
+        { userID: 1, date: '2019/06/28', numOunces: 44 },
+        { userID: 1, date: '2019/06/29', numOunces: 51 }
+      ]);
     });
   });
 
