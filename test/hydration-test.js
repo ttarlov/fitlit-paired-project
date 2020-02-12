b6fb096d7582811e3be58f8594b813d62f9fc6fc
Taras Tarlov <ttarlov@gmail.com>
2020-02-12 16:34:49 -0700

    Clean up syntax

--- test/hydration-test.js
+++ test/hydration-test.js
@@ -4,8 +4,7 @@ const Hydration = require('../src/hydration.js');
 const User = require('../src/user.js');
 
 beforeEach(() => {
-  data = [
-    {
+  data = [{
       "userID": 2,
       "date": "2019/06/15",
       "numOunces": 55
@@ -140,25 +139,79 @@ describe('Hydration', function() {
   describe('calculateWeeklyOz Method', () => {
 
     it('should calculate how many fl oz consumed each day over the course of a week', () => {
-      expect(hydro.calculateWeeklyOz('2019/06/24')).to.deep.equal([
-        { userID: 2, date: '2019/06/17', numOunces: 73 },
-        { userID: 2, date: '2019/06/18', numOunces: 85 },
-        { userID: 2, date: '2019/06/19', numOunces: 93 },
-        { userID: 2, date: '2019/06/20', numOunces: 29 },
-        { userID: 2, date: '2019/06/22', numOunces: 30 },
-        { userID: 2, date: '2019/06/23', numOunces: 45 },
-        { userID: 2, date: '2019/06/24', numOunces: 48 }
+      expect(hydro.calculateWeeklyOz('2019/06/24')).to.deep.equal([{
+          userID: 2,
+          date: '2019/06/17',
+          numOunces: 73
+        },
+        {
+          userID: 2,
+          date: '2019/06/18',
+          numOunces: 85
+        },
+        {
+          userID: 2,
+          date: '2019/06/19',
+          numOunces: 93
+        },
+        {
+          userID: 2,
+          date: '2019/06/20',
+          numOunces: 29
+        },
+        {
+          userID: 2,
+          date: '2019/06/22',
+          numOunces: 30
+        },
+        {
+          userID: 2,
+          date: '2019/06/23',
+          numOunces: 45
+        },
+        {
+          userID: 2,
+          date: '2019/06/24',
+          numOunces: 48
+        }
       ]);
     });
     it('should calculate how many fl oz consumed each day over the course of a week for another user', () => {
-      expect(hydro2.calculateWeeklyOz('2019/06/29')).to.deep.equal([
-        { userID: 1, date: '2019/06/23', numOunces: 28 },
-        { userID: 1, date: '2019/06/24', numOunces: 40 },
-        { userID: 1, date: '2019/06/25', numOunces: 33 },
-        { userID: 1, date: '2019/06/26', numOunces: 29 },
-        { userID: 1, date: '2019/06/27', numOunces: 37 },
-        { userID: 1, date: '2019/06/28', numOunces: 44 },
-        { userID: 1, date: '2019/06/29', numOunces: 51 }
+      expect(hydro2.calculateWeeklyOz('2019/06/29')).to.deep.equal([{
+          userID: 1,
+          date: '2019/06/23',
+          numOunces: 28
+        },
+        {
+          userID: 1,
+          date: '2019/06/24',
+          numOunces: 40
+        },
+        {
+          userID: 1,
+          date: '2019/06/25',
+          numOunces: 33
+        },
+        {
+          userID: 1,
+          date: '2019/06/26',
+          numOunces: 29
+        },
+        {
+          userID: 1,
+          date: '2019/06/27',
+          numOunces: 37
+        },
+        {
+          userID: 1,
+          date: '2019/06/28',
+          numOunces: 44
+        },
+        {
+          userID: 1,
+          date: '2019/06/29',
+          numOunces: 51
+        }
       ]);
     });
   });
