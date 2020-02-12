85511483a3b36a1804d4b8f2f29f3a83c766bb3d
Taras Tarlov <ttarlov@gmail.com>
2020-02-11 22:35:22 -0700

    :bar_chart: Put proper indentatiot. Add test for second user for weekly activity

--- test/activity-test.js
+++ test/activity-test.js
@@ -6,189 +6,187 @@ const UserRepository = require('../src/userrepository.js');
 let data, sampleUserData1
 
 beforeEach(() => {
-  data = [
-{
-  "userID": 1,
-  "date": "2019/06/15",
-  "numSteps": 3577,
-  "minutesActive": 140,
-  "flightsOfStairs": 16
-},
-{
-  "userID": 1,
-  "date": "2019/06/16",
-  "numSteps": 4294,
-  "minutesActive": 138,
-  "flightsOfStairs": 10
-},
-{
-  "userID": 1,
-  "date": "2019/06/17",
-  "numSteps": 7402,
-  "minutesActive": 116,
-  "flightsOfStairs": 33
-},
-{
-  "userID": 1,
-  "date": "2019/06/18",
-  "numSteps": 3486,
-  "minutesActive": 114,
-  "flightsOfStairs": 32
-},
-{
-  "userID": 1,
-  "date": "2019/06/19",
-  "numSteps": 11374,
-  "minutesActive": 213,
-  "flightsOfStairs": 13
-},
-{
-  "userID": 1,
-  "date": "2019/06/20",
-  "numSteps": 14810,
-  "minutesActive": 287,
-  "flightsOfStairs": 18
-},
-{
-  "userID": 1,
-  "date": "2019/06/21",
-  "numSteps": 2634,
-  "minutesActive": 107,
-  "flightsOfStairs": 5
-},
-{
-  "userID": 1,
-  "date": "2019/06/22",
-  "numSteps": 10333,
-  "minutesActive": 114,
-  "flightsOfStairs": 31
-},
-{
-  "userID": 1,
-  "date": "2019/06/23",
-  "numSteps": 6389,
-  "minutesActive": 41,
-  "flightsOfStairs": 33
-},
-{
-  "userID": 1,
-  "date": "2019/06/24",
-  "numSteps": 8015,
-  "minutesActive": 106,
-  "flightsOfStairs": 37
-},
-{
-  "userID": 2,
-  "date": "2019/06/15",
-  "numSteps": 11652,
-  "minutesActive": 20,
-  "flightsOfStairs": 24
-},
-{
-  "userID": 2,
-  "date": "2019/06/16",
-  "numSteps": 9256,
-  "minutesActive": 108,
-  "flightsOfStairs": 2
-},
-{
-  "userID": 2,
-  "date": "2019/06/17",
-  "numSteps": 9303,
-  "minutesActive": 27,
-  "flightsOfStairs": 14
-},
-{
-  "userID": 2,
-  "date": "2019/06/18",
-  "numSteps": 8024,
-  "minutesActive": 216,
-  "flightsOfStairs": 32
-},
-{
-  "userID": 2,
-  "date": "2019/06/19",
-  "numSteps": 14113,
-  "minutesActive": 80,
-  "flightsOfStairs": 10
-},
-{
-  "userID": 2,
-  "date": "2019/06/20",
-  "numSteps": 6188,
-  "minutesActive": 292,
-  "flightsOfStairs": 32
-},
-{
-  "userID": 2,
-  "date": "2019/06/21",
-  "numSteps": 12982,
-  "minutesActive": 231,
-  "flightsOfStairs": 55
-},
-{
-  "userID": 2,
-  "date": "2019/06/22",
-  "numSteps": 13881,
-  "minutesActive": 137,
-  "flightsOfStairs": 46
-},
-{
-  "userID": 2,
-  "date": "2019/06/23",
-  "numSteps": 5683,
-  "minutesActive": 210,
-  "flightsOfStairs": 41
-},
-{
-  "userID": 2,
-  "date": "2019/06/24",
-  "numSteps": 9052,
-  "minutesActive": 168,
-  "flightsOfStairs": 14
-}
-];
+  data = [{
+      "userID": 1,
+      "date": "2019/06/15",
+      "numSteps": 3577,
+      "minutesActive": 140,
+      "flightsOfStairs": 16
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/16",
+      "numSteps": 4294,
+      "minutesActive": 138,
+      "flightsOfStairs": 10
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/17",
+      "numSteps": 7402,
+      "minutesActive": 116,
+      "flightsOfStairs": 33
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/18",
+      "numSteps": 3486,
+      "minutesActive": 114,
+      "flightsOfStairs": 32
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/19",
+      "numSteps": 11374,
+      "minutesActive": 213,
+      "flightsOfStairs": 13
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/20",
+      "numSteps": 14810,
+      "minutesActive": 287,
+      "flightsOfStairs": 18
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/21",
+      "numSteps": 2634,
+      "minutesActive": 107,
+      "flightsOfStairs": 5
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/22",
+      "numSteps": 10333,
+      "minutesActive": 114,
+      "flightsOfStairs": 31
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/23",
+      "numSteps": 6389,
+      "minutesActive": 41,
+      "flightsOfStairs": 33
+    },
+    {
+      "userID": 1,
+      "date": "2019/06/24",
+      "numSteps": 8015,
+      "minutesActive": 106,
+      "flightsOfStairs": 37
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/15",
+      "numSteps": 11652,
+      "minutesActive": 20,
+      "flightsOfStairs": 24
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/16",
+      "numSteps": 9256,
+      "minutesActive": 108,
+      "flightsOfStairs": 2
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/17",
+      "numSteps": 9303,
+      "minutesActive": 27,
+      "flightsOfStairs": 14
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/18",
+      "numSteps": 8024,
+      "minutesActive": 216,
+      "flightsOfStairs": 32
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/19",
+      "numSteps": 14113,
+      "minutesActive": 80,
+      "flightsOfStairs": 10
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/20",
+      "numSteps": 6188,
+      "minutesActive": 292,
+      "flightsOfStairs": 32
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/21",
+      "numSteps": 12982,
+      "minutesActive": 231,
+      "flightsOfStairs": 55
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/22",
+      "numSteps": 13881,
+      "minutesActive": 137,
+      "flightsOfStairs": 46
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/23",
+      "numSteps": 5683,
+      "minutesActive": 210,
+      "flightsOfStairs": 41
+    },
+    {
+      "userID": 2,
+      "date": "2019/06/24",
+      "numSteps": 9052,
+      "minutesActive": 168,
+      "flightsOfStairs": 14
+    }
+  ];
 
-sampleUserData = [
-  {
-    "id": 1,
-    "name": "Luisa Hane",
-    "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
-    "email": "Diana.Hayes1@hotmail.com",
-    "strideLength": 4.3,
-    "dailyStepGoal": 10000,
-    "friends": [
-      16,
-      4,
-      8
-    ]
-  },
-  {
-    "id": 2,
-    "name": "Jarvis Considine",
-    "address": "30086 Kathryn Port, Ciceroland NE 07273",
-    "email": "Dimitri.Bechtelar11@gmail.com",
-    "strideLength": 4.5,
-    "dailyStepGoal": 5000,
-    "friends": [
-      9,
-      18,
-      24,
-      19
-    ]
-  },
-];
-activity = new Activity(data, 1)
-activity2 = new Activity(data, 2)
+  sampleUserData = [{
+      "id": 1,
+      "name": "Luisa Hane",
+      "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+      "email": "Diana.Hayes1@hotmail.com",
+      "strideLength": 4.3,
+      "dailyStepGoal": 10000,
+      "friends": [
+        16,
+        4,
+        8
+      ]
+    },
+    {
+      "id": 2,
+      "name": "Jarvis Considine",
+      "address": "30086 Kathryn Port, Ciceroland NE 07273",
+      "email": "Dimitri.Bechtelar11@gmail.com",
+      "strideLength": 4.5,
+      "dailyStepGoal": 5000,
+      "friends": [
+        9,
+        18,
+        24,
+        19
+      ]
+    },
+  ];
+  activity = new Activity(data, 1)
+  activity2 = new Activity(data, 2)
 });
 
 describe('Activity', function() {
 
-  it('should be a function', function(){
+  it('should be a function', function() {
     expect(Activity).to.be.a('function');
   });
 
-  it('should be an instance of Activity', function(){
+  it('should be an instance of Activity', function() {
     expect(activity).to.be.an.instanceof(Activity);
   });
 
@@ -247,12 +245,12 @@ describe('Activity', function() {
   describe('findDaysStepGoalWasExceeded method', () => {
     it('should tell whether a user reached their daily step goal', () => {
       let users = new UserRepository(sampleUserData)
-      expect(activity.findDaysStepGoalWasExceeded( users.getUserDataById(1))).to.deep.equal([ '2019/06/19', '2019/06/20', '2019/06/22' ])
+      expect(activity.findDaysStepGoalWasExceeded(users.getUserDataById(1))).to.deep.equal(['2019/06/19', '2019/06/20', '2019/06/22'])
     });
 
     it('should tell whether another user reached their daily step goal', () => {
       let users = new UserRepository(sampleUserData)
-      expect(activity.findDaysStepGoalWasExceeded( users.getUserDataById(2))).to.deep.equal(['2019/06/17','2019/06/19','2019/06/20','2019/06/22','2019/06/23','2019/06/24'])
+      expect(activity.findDaysStepGoalWasExceeded(users.getUserDataById(2))).to.deep.equal(['2019/06/17', '2019/06/19', '2019/06/20', '2019/06/22', '2019/06/23', '2019/06/24'])
     });
   });
 
@@ -309,6 +307,115 @@ describe('Activity', function() {
     });
   });
 
+  describe('findWeeklyActivity method', function() {
+    it('Find users all weekly activity data', function() {
+      expect(activity.findWeeklyActivity('2019/06/24')).to.deep.equal([{
+          userID: 1,
+          date: '2019/06/18',
+          numSteps: 3486,
+          minutesActive: 114,
+          flightsOfStairs: 32
+        },
+        {
+          userID: 1,
+          date: '2019/06/19',
+          numSteps: 11374,
+          minutesActive: 213,
+          flightsOfStairs: 13
+        },
+        {
+          userID: 1,
+          date: '2019/06/20',
+          numSteps: 14810,
+          minutesActive: 287,
+          flightsOfStairs: 18
+        },
+        {
+          userID: 1,
+          date: '2019/06/21',
+          numSteps: 2634,
+          minutesActive: 107,
+          flightsOfStairs: 5
+        },
+        {
+          userID: 1,
+          date: '2019/06/22',
+          numSteps: 10333,
+          minutesActive: 114,
+          flightsOfStairs: 31
+        },
+        {
+          userID: 1,
+          date: '2019/06/23',
+          numSteps: 6389,
+          minutesActive: 41,
+          flightsOfStairs: 33
+        },
+        {
+          userID: 1,
+          date: '2019/06/24',
+          numSteps: 8015,
+          minutesActive: 106,
+          flightsOfStairs: 37
+        }
+      ])
+    });
+
+
+    it('Find another users all weekly activity data', function() {
+      expect(activity2.findWeeklyActivity('2019/06/24')).to.deep.equal([{
+          userID: 2,
+          date: '2019/06/18',
+          numSteps: 8024,
+          minutesActive: 216,
+          flightsOfStairs: 32
+        },
+        {
+          userID: 2,
+          date: '2019/06/19',
+          numSteps: 14113,
+          minutesActive: 80,
+          flightsOfStairs: 10
+        },
+        {
+          userID: 2,
+          date: '2019/06/20',
+          numSteps: 6188,
+          minutesActive: 292,
+          flightsOfStairs: 32
+        },
+        {
+          userID: 2,
+          date: '2019/06/21',
+          numSteps: 12982,
+          minutesActive: 231,
+          flightsOfStairs: 55
+        },
+        {
+          userID: 2,
+          date: '2019/06/22',
+          numSteps: 13881,
+          minutesActive: 137,
+          flightsOfStairs: 46
+        },
+        {
+          userID: 2,
+          date: '2019/06/23',
+          numSteps: 5683,
+          minutesActive: 210,
+          flightsOfStairs: 41
+        },
+        {
+          userID: 2,
+          date: '2019/06/24',
+          numSteps: 9052,
+          minutesActive: 168,
+          flightsOfStairs: 14
+        }
+      ])
+    });
+
+  });
 
 });
 
