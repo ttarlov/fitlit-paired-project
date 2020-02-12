82cab968d8066ae80da575cd390423b9a470de0c
Taras Tarlov <ttarlov@gmail.com>
2020-02-12 16:34:38 -0700

    Clean up syntax

--- src/sleep.js
+++ src/sleep.js
@@ -3,7 +3,7 @@ class Sleep {
     this.data = sleepData;
     this.id = id;
     this.user = this.findUserSleepData();
-  };
+  }
 
   findUserSleepData() {
     return this.data.filter(user => user.userID === this.id);
@@ -11,10 +11,10 @@ class Sleep {
 
   calculateAvgSleepQuality() {
     let totalSleep = this.user.reduce((total, day) => {
-        total += day.sleepQuality
-        return total
-      },0);
-    return (totalSleep/this.user.length).toFixed(2);
+      total += day.sleepQuality
+      return total
+    }, 0);
+    return (totalSleep / this.user.length).toFixed(2);
   }
 
   calculateHoursSleptByDay(date) {
@@ -29,39 +29,39 @@ class Sleep {
 
   calculateWeeklySleep(date) {
     let indexOfCurrentDay = this.user.findIndex(day => day.date === date);
-      return this.user.slice(indexOfCurrentDay -6, indexOfCurrentDay + 1)
-  };
+    return this.user.slice(indexOfCurrentDay - 6, indexOfCurrentDay + 1)
+  }
 
   calculateAvgSleepQualityForAllUsers() {
     let totalSleepQuality = this.data.reduce((acc, user) => {
-          acc+= user.sleepQuality
-          return acc;
-        },0);
-      return  Number((totalSleepQuality / this.data.length).toFixed(2));
-  };
+      acc += user.sleepQuality
+      return acc;
+    }, 0);
+    return Number((totalSleepQuality / this.data.length).toFixed(2));
+  }
 
   findMostRestedUserByDate(date) {
     let selectedDay = this.data.filter(day => day.date === date)
     return selectedDay.sort((a, b) => b.hoursSlept - a.hoursSlept)[0]
   }
 
-  //custom metric
+
   calculatePercentageOfRecommendedSleep(date) {
     let indexOfCurrentDay = this.user.findIndex(day => day.date === date);
-    let weeklySleep = this.user.slice(indexOfCurrentDay -6, indexOfCurrentDay + 1)
+    let weeklySleep = this.user.slice(indexOfCurrentDay - 6, indexOfCurrentDay + 1)
     let totalHours = weeklySleep.reduce((acc, hours) => {
-      acc+= hours.hoursSlept;
+      acc += hours.hoursSlept;
       return acc
-    },0)
+    }, 0)
     return `${((totalHours / 56) * 100).toFixed(2)}%`
   }
 
   calculateTotalAvgHoursSlept() {
     let totalSleep = this.user.reduce((total, day) => {
-        total += day.hoursSlept
-        return total
-      },0);
-    return (totalSleep/this.user.length).toFixed(2);
+      total += day.hoursSlept
+      return total
+    }, 0);
+    return (totalSleep / this.user.length).toFixed(2);
   }
 }
 
